9631a6fe230303cf3226f1e5cd1bce554594a168
nk <nk@localhost.localdomain>
2023-10-20 05:06:49 -0400

    update

---
+++ init.sh
@@ -0,0 +1,56 @@
+ #!/bin/bash 
+
+ # bash script to automatically config my config from github 
+
+
+# Bold High Intensity
+BIBlack='\033[1;90m'      # Black
+BIRed='\033[1;91m'        # Red
+BIGreen='\033[1;92m'      # Green
+BIYellow='\033[1;93m'     # Yellow
+BIBlue='\033[1;94m'       # Blue
+BIPurple='\033[1;95m'     # Purple
+BICyan='\033[1;96m'       # Cyan
+BIWhite='\033[1;97m'      # White
+Color_Off='\033[0m'       # Text Reset
+
+# Functions to run 
+
+Base_Prog(){ 
+  sudo zypper -y git ripgrep gcc gcc-c++ >> Install.log 
+  rm -r Install.log 
+}
+
+nvim(){
+  mv nvim ~/.config/    
+}
+zsh(){
+  mv .zshrc ~/ 
+}
+kitty(){
+mv kitty ~/.config/    
+}
+
+
+echo -e "${BIGreen}Please Choose What to Install${Color_Off}"
+read -p "${BIGreen}1.Neovim\n2.zsh\n3.Kitty"${Color_Off}" prog 
+
+if [[ -z $prog ]]
+then 
+  echo -p "${BIRed}No Input Detected${Color_Off}"
+else 
+  case "$prog" in
+    1) 
+      nvim
+      ;;
+    2) 
+      zsh  
+      ;;
+    3) 
+      kitty 
+      ;;
+    *)
+      echo "${BIRed}Invalid choice please try again!!${Color_Off}"
+      ;;
+  esac
+fi 
