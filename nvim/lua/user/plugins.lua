2a90870020c34765633aec0f9eb23fd4f11b4c6b
nk <nk@localhost.localdomain>
2023-11-06 14:29:00 -0500

    Update

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -125,29 +125,6 @@ require('lazy').setup({
   -- refactoring 
    'nvim-pack/nvim-spectre',
 
-  -- note taking 
-{
-  "nvim-neorg/neorg",
-  build = ":Neorg sync-parsers",
-  dependencies = { "nvim-lua/plenary.nvim" },
-  config = function()
-    require("neorg").setup {
-      load = {
-        ["core.defaults"] = {}, -- Loads default behaviour
-        ["core.export"] = {}, 
-        ["core.concealer"] = {}, -- Adds pretty icons to your documents
-        ["core.dirman"] = { -- Manages Neorg workspaces
-          config = {
-            workspaces = {
-              notes = "~/notes",
-            },
-            default_workspace = "notes",
-          },
-        },
-      },
-    }
-  end,
-},
   -- Writing 
    { 
     "junegunn/goyo.vim", 
